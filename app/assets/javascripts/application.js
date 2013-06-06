28bf1b59988ba16f14cda1bc6e51771dd475ae24
James Keane <mail@james-keane.com>
2013-06-06 21:22:58 +0100

    Inital commit with ported template

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require vendor/wrap/jquery-1.8.2.min
+//= require vendor/wrap/modernizr.custom.17475
+//= require vendor/bootstrap/bootstrap.min
+//= require vendor/fancybox/jquery.fancybox.pack
+//= require vendor/elastislide/jquery.elastislide
+//= require vendor/flexslider/jquery.flexslider-min
+//= require vendor/parallax-slider/modernizr
+//= require vendor/parallax-slider/jquery.cslider
+//= require vendor/bxslider/jquery.bxslider
+//= require vendor/wrap/app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LingokeyDynamic::Application.config.secret_token = 'edc44618f641fd81f9fdd6186d3efdb1655b400e8e527619e0f140b53edddaa22f184833246d9304883d592fcdfb22b25239cde71c362b7b9cc0cca74742ce91'
