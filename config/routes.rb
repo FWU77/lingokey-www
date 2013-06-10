9977ad4d6420edaa0d8bba29011db0704eac5222
James Keane <mail@james-keane.com>
2013-06-10 10:53:55 +0100

    Changed Menu for live

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 LingokeyDynamic::Application.routes.draw do
   root :to => "static_pages#home"
 
-  get "static_pages/home"
+  match 'contact' => 'static_pages#contact'
 
-  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
