364fb83df7a27a72b060571dc1345269c6f56c99
MalcolmPJohnston <malcolm.johnston@lingo-key.com>
2014-02-13 12:23:58 +0000

    Remove Flash Message on successful send of message.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,7 +9,7 @@ class ContactController < ApplicationController
     
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
-      redirect_to('/thankyou', :notice => "Message was successfully sent.")
+      redirect_to('/thankyou')
     else
       flash.now.alert = "Please fill all fields."
       render :new
